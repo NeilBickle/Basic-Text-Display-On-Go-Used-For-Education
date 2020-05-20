85835dd194d9e5b10beb3c957f304d61f55ff966
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2020-05-20 08:33:35 +0100

    Added Main.go
    1. Added Main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Have a go at guessing what languages these lines are from once you have guessed them add it to the issues page, Please Note Google Translate was used for the translation if you get extremely stuck you can use that for a hint.")
+	fmt.Println("-------------------------------------------------")
+	fmt.Println("Hello Go")
+	fmt.Println("Kon'nichiwa iku")
+	fmt.Println("Hallo geh")
+	fmt.Println("Hola ir")
+}
